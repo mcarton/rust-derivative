bc27f92ce37f33e2e853dc9c54909ccd087f0d59
mcarton <cartonmartin+git@gmail.com>
2016-10-08 02:47:30 +0200

    Add some documentation

--- src/attr.rs
+++ src/attr.rs
@@ -1,36 +1,52 @@
 use syn;
 
+/// Represent the `derivative` attributes on the input type (`struct`/`enum`).
 #[derive(Debug, Default)]
 pub struct Input {
+    /// Whether `Debug` is present and its specific attributes.
     pub debug: Option<InputDebug>,
+    /// Whether `Default` is present.
     pub default: bool,
 }
 
 #[derive(Debug, Default)]
+/// Represent the `derivative` attributes on a field.
 pub struct Field {
+    /// Whether `Debug` is present and its specific attributes.
     debug: Option<FieldDebug>,
+    /// Whether `Default` is present and its specific attributes.
     default: Option<FieldDefault>,
 }
 
 #[derive(Debug, Default)]
+/// Represent the `derivative(Debug(…))` attributes on an input.
 pub struct InputDebug {
+    /// The `bound` attribute if present and the corresponding bounds.
     bounds: Option<Vec<syn::WherePredicate>>,
+    /// Whether the type is marked `transparent`.
     pub transparent: bool,
 }
 
 #[derive(Debug, Default)]
+/// Represents the `derivarive(Debug(…))` attributes on a field.
 pub struct FieldDebug {
+    /// The `bound` attribute if present and the corresponding bounds.
     bounds: Option<Vec<syn::WherePredicate>>,
+    /// The `format_with` attribute if present and the path to the formatting function.
     format_with: Option<syn::Path>,
+    /// Whether the field is to be ignore from output.
     ignore: bool,
 }
 
 #[derive(Debug, Default)]
+/// Represent the `derivarive(Default(…))` attributes on a field.
 pub struct FieldDefault {
+    /// The default value for the field if present.
     pub value: Option<syn::Expr>,
 }
 
 impl Input {
+    /// Parse the `derivative` attributes on a type.
     pub fn from_ast(attrs: &[syn::Attribute]) -> Result<Input, String> {
         let mut input = Input::default();
 
@@ -78,6 +94,7 @@ impl Input {
 }
 
 impl Field {
+    /// Parse the `derivative` attributes on a type.
     pub fn from_ast(field: &syn::Field) -> Result<Field, String> {
         let mut out = Field::default();
 
@@ -157,6 +174,7 @@ impl Field {
 /// * `#[derivative(Debug(foo="bar")]` is represented as `("Debug", [("foo", Some("bar"))])`.
 struct MetaItem<'a>(&'a str, Vec<(&'a str, Option<&'a str>)>);
 
+/// Parse an arbitrary item for our limited `MetaItem` subset.
 fn read_items(item: &syn::MetaItem) -> Result<MetaItem, String> {
     match *item {
         syn::MetaItem::Word(ref name) => Ok(MetaItem(name.as_ref(), Vec::new())),
@@ -189,6 +207,7 @@ fn read_items(item: &syn::MetaItem) -> Result<MetaItem, String> {
     }
 }
 
+/// Filter the `derivative` items from an attribute.
 fn derivative_attribute(attr: &syn::Attribute) -> Option<&[syn::MetaItem]> {
     match attr.value {
         syn::MetaItem::List(ref name, ref mis) if name == "derivative" => {
@@ -200,6 +219,10 @@ fn derivative_attribute(attr: &syn::Attribute) -> Option<&[syn::MetaItem]> {
     }
 }
 
+/// Parse an item value as a boolean. Accepted values are the string literal `"true"` and
+/// `"false"`. The `default` parameter specifies what the value of the boolean is when only its
+/// name is specified (eg. `Debug="ignore"` is equivalent to `Debug(ignore="true")`). The `name`
+/// parameter is used for error reporting.
 fn parse_boolean_meta_item(item: &Option<&str>, default: bool, name: &str) -> Result<bool, String> {
     match *item {
         Some("true") => Ok(true),
@@ -209,6 +232,7 @@ fn parse_boolean_meta_item(item: &Option<&str>, default: bool, name: &str) -> Re
     }
 }
 
+/// Parse a `bound` item.
 fn parse_bound(bounds: &mut Vec<syn::WherePredicate>, value: Option<&str>) -> Result<(), String> {
     let bound = try!(value.ok_or_else(|| "`bound` needs a value".to_string()));
     let where_clause = syn::parse_where_clause(&format!("where {}", bound));
@@ -217,6 +241,7 @@ fn parse_bound(bounds: &mut Vec<syn::WherePredicate>, value: Option<&str>) -> Re
     Ok(())
 }
 
+/// Get the string out of a string literal or report an error for other literals.
 fn str_or_err(lit: &syn::Lit) -> Result<&str, String> {
     if let syn::Lit::Str(ref value, _) = *lit {
         Ok(value.as_str())
