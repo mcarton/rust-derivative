cb36c28d80c6f406a8e22807804189a9c6c13b55
David Tolnay <dtolnay@palantir.com>
2016-11-29 17:20:56 -0800

    Let rust ignore attributes

--- src/utils.rs
+++ src/utils.rs
@@ -3,49 +3,6 @@ use attr;
 use bound;
 use syn;
 
-pub fn derivative_attribute(attr: &syn::Attribute) -> Option<&[syn::MetaItem]> {
-    match attr.value {
-        syn::MetaItem::List(ref name, ref mis) if name == "derivative" => {
-            Some(mis)
-        }
-        syn::MetaItem::Word(..) |
-        syn::MetaItem::NameValue(..) |
-        syn::MetaItem::List(..) => None,
-    }
-}
-
-pub fn remove_derivative_attrs(input: &mut syn::MacroInput) {
-    fn remove_from_vec(attrs: &mut Vec<syn::Attribute>) {
-        attrs.retain(|attr| derivative_attribute(attr).is_none());
-    }
-
-    fn remove_from_variant_data(vd: &mut syn::VariantData) {
-        match *vd {
-            syn::VariantData::Struct(ref mut fields) |
-            syn::VariantData::Tuple(ref mut fields) => {
-                for field in fields {
-                    remove_from_vec(&mut field.attrs);
-                }
-            }
-            syn::VariantData::Unit => (),
-        }
-    }
-
-    remove_from_vec(&mut input.attrs);
-
-    match input.body {
-        syn::Body::Enum(ref mut variants) => {
-            for variant in variants {
-                remove_from_vec(&mut variant.attrs);
-                remove_from_variant_data(&mut variant.data);
-            }
-        }
-        syn::Body::Struct(ref mut vd) => {
-            remove_from_variant_data(vd);
-        }
-    }
- }
-
 /// Make generic with all the generics in the input, plus a bound `T: <trait_path>` for each
 /// generic field type that will be shown.
 pub fn build_impl_generics<F, G, H>(
