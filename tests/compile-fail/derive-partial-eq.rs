9ff0fc5ff874a9bab8ea07c399c75f873387bc72
Azriel Hoh <azriel91@gmail.com>
2018-10-18 14:47:07 +1300

    Fixed derive-partial-eq compile failure test.
    Issue #25

--- tests/compile-fail/derive-partial-eq.rs
+++ tests/compile-fail/derive-partial-eq.rs
@@ -2,7 +2,7 @@
 extern crate derivative;
 
 #[derive(Derivative)]
-//~^ ERROR custom derive attribute panicked
+//~^ ERROR 4:10: 4:20: proc-macro derive panicked
 //~| HELP can't use `#[derivative(PartialEq)]` on an enumeration without `feature_allow_slow_enum`
 #[derivative(PartialEq)]
 enum Option<T> {
