108b68ebe5df53aceeeb62aa5aeca0b806c44ae9
mcarton <cartonmartin+git@gmail.com>
2019-08-29 00:02:38 +0200

    Import tests from rustc for comparison derives

--- tests/rustc-deriving-cmp-generic-struct-enum.rs
+++ tests/rustc-deriving-cmp-generic-struct-enum.rs
@@ -19,46 +19,49 @@ extern crate derivative;
 #[derive(Derivative)]
 #[derivative(PartialEq = "feature_allow_slow_enum", Eq, PartialOrd = "feature_allow_slow_enum")]
 #[derive(Ord)]
-enum E<T> {
-    E0,
-    E1(T),
-    E2(T, T),
+enum ES<T> {
+    ES1 { x: T },
+    ES2 { x: T, y: T }
 }
 
-#[test]
-fn main() {
-    let e0 = E::E0;
-    let e11 = E::E1(1);
-    let e12 = E::E1(2);
-    let e21 = E::E2(1, 1);
-    let e22 = E::E2(1, 2);
+
+pub fn main() {
+    let (es11, es12, es21, es22) = (ES::ES1 {
+        x: 1
+    }, ES::ES1 {
+        x: 2
+    }, ES::ES2 {
+        x: 1,
+        y: 1
+    }, ES::ES2 {
+        x: 1,
+        y: 2
+    });
 
     // in order for both PartialOrd and Ord
-    let es = [e0, e11, e12, e21, e22];
+    let ess = [es11, es12, es21, es22];
 
-    for (i, e1) in es.iter().enumerate() {
-        for (j, e2) in es.iter().enumerate() {
+    for (i, es1) in ess.iter().enumerate() {
+        for (j, es2) in ess.iter().enumerate() {
             let ord = i.cmp(&j);
 
             let eq = i == j;
-            let lt = i < j;
-            let le = i <= j;
-            let gt = i > j;
-            let ge = i >= j;
+            let (lt, le) = (i < j, i <= j);
+            let (gt, ge) = (i > j, i >= j);
 
             // PartialEq
-            assert_eq!(*e1 == *e2, eq);
-            assert_eq!(*e1 != *e2, !eq);
+            assert_eq!(*es1 == *es2, eq);
+            assert_eq!(*es1 != *es2, !eq);
 
             // PartialOrd
-            assert_eq!(*e1 < *e2, lt);
-            assert_eq!(*e1 > *e2, gt);
+            assert_eq!(*es1 < *es2, lt);
+            assert_eq!(*es1 > *es2, gt);
 
-            assert_eq!(*e1 <= *e2, le);
-            assert_eq!(*e1 >= *e2, ge);
+            assert_eq!(*es1 <= *es2, le);
+            assert_eq!(*es1 >= *es2, ge);
 
             // Ord
-            assert_eq!(e1.cmp(e2), ord);
+            assert_eq!(es1.cmp(es2), ord);
         }
     }
 }
--- tests/rustc-deriving-cmp-generic-struct.rs
+++ tests/rustc-deriving-cmp-generic-struct.rs
@@ -16,28 +16,23 @@ extern crate core;
 #[macro_use]
 extern crate derivative;
 
-#[derive(Derivative)]
-#[derivative(PartialEq = "feature_allow_slow_enum", Eq, PartialOrd = "feature_allow_slow_enum")]
 #[derive(Ord)]
-enum E<T> {
-    E0,
-    E1(T),
-    E2(T, T),
+#[derive(Derivative)]
+#[derivative(PartialEq, Eq, PartialOrd)]
+struct S<T> {
+    x: T,
+    y: T
 }
 
-#[test]
-fn main() {
-    let e0 = E::E0;
-    let e11 = E::E1(1);
-    let e12 = E::E1(2);
-    let e21 = E::E2(1, 1);
-    let e22 = E::E2(1, 2);
+pub fn main() {
+    let s1 = S {x: 1, y: 1};
+    let s2 = S {x: 1, y: 2};
 
     // in order for both PartialOrd and Ord
-    let es = [e0, e11, e12, e21, e22];
+    let ss = [s1, s2];
 
-    for (i, e1) in es.iter().enumerate() {
-        for (j, e2) in es.iter().enumerate() {
+    for (i, s1) in ss.iter().enumerate() {
+        for (j, s2) in ss.iter().enumerate() {
             let ord = i.cmp(&j);
 
             let eq = i == j;
@@ -47,18 +42,18 @@ fn main() {
             let ge = i >= j;
 
             // PartialEq
-            assert_eq!(*e1 == *e2, eq);
-            assert_eq!(*e1 != *e2, !eq);
+            assert_eq!(*s1 == *s2, eq);
+            assert_eq!(*s1 != *s2, !eq);
 
             // PartialOrd
-            assert_eq!(*e1 < *e2, lt);
-            assert_eq!(*e1 > *e2, gt);
+            assert_eq!(*s1 < *s2, lt);
+            assert_eq!(*s1 > *s2, gt);
 
-            assert_eq!(*e1 <= *e2, le);
-            assert_eq!(*e1 >= *e2, ge);
+            assert_eq!(*s1 <= *s2, le);
+            assert_eq!(*s1 >= *s2, ge);
 
             // Ord
-            assert_eq!(e1.cmp(e2), ord);
+            assert_eq!(s1.cmp(s2), ord);
         }
     }
 }
