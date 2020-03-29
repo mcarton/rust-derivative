15dcd66c1017c94ebe445afe5fd47f422101203c
Martin Carton <martin.carton@prodrive-technologies.com>
2020-03-29 21:00:35 +0200

    Fix test for `feature_allow_slow_enum`

--- tests/compile-fail/derive-partial-ord.rs
+++ tests/compile-fail/derive-partial-ord.rs
@@ -3,8 +3,8 @@ extern crate derivative;
 
 #[derive(Derivative)]
 //~^ ERROR 4:10: 4:20: proc-macro derive panicked
-//~| HELP can't use `#[derivative(PartialEq)]` on an enumeration without `feature_allow_slow_enum`
-#[derivative(PartialEq)]
+//~| HELP can't use `#[derivative(PartialOrd)]` on an enumeration without `feature_allow_slow_enum`
+#[derivative(PartialOrd)]
 enum Option<T> {
     Some(T),
     None,
