850be84a2807309dbefd5ae57c797a0e3e676b96
Martin Carton <martin.carton@prodrive-technologies.com>
2020-04-07 21:43:49 +0200

    Ignore compile-tests on non-master
    The format of the error message changes from one version of the compiler
    to another.

--- tests/compile-test.rs
+++ tests/compile-test.rs
@@ -1,6 +1,7 @@
 extern crate trybuild;
 
 #[test]
+#[ignore]
 fn compile_test() {
     let t = trybuild::TestCases::new();
     let pattern = std::env::var("DERIVATIVE_TEST_FILTER").unwrap_or_else(|_| String::from("*.rs"));
