5ece207ce74c58cc2b55d0ad9c692f3da906df6a
Martin Carton <martin.carton@prodrive-technologies.com>
2021-01-19 21:57:38 +0100

    Improve spans when deriving `Hash`

--- src/hash.rs
+++ src/hash.rs
@@ -1,20 +1,21 @@
-use proc_macro2;
-
 use ast;
 use attr;
 use matcher;
 use paths;
+use proc_macro2::{self, TokenStream};
 use syn;
 use utils;
 
 pub fn derive(input: &ast::Input) -> proc_macro2::TokenStream {
     let hasher_trait_path = hasher_trait_path();
-    let hash_trait_path = hash_trait_path();
+    let hash_trait_path_input_span = hash_trait_path(input.span);
+
+    let state = quote!(__state);
 
     let discriminant = if let ast::Body::Enum(_) = input.body {
         let discriminant = paths::discriminant_path();
         Some(quote!(
-            #hash_trait_path::hash(&#discriminant(self), __state);
+            #hash_trait_path_input_span::hash(&#discriminant(self), #state);
         ))
     } else {
         None
@@ -33,11 +34,13 @@ pub fn derive(input: &ast::Input) -> proc_macro2::TokenStream {
 
                 if let Some(hash_with) = bi.field.attrs.hash_with() {
                     Some(quote! {
-                        #hash_with(&#arg, __state);
+                        #hash_with(&#arg, #state);
                     })
                 } else {
-                    Some(quote! {
-                        #hash_trait_path::hash(&#arg, __state);
+                    let hash_trait_path = hash_trait_path(bi.field.span);
+
+                    Some(quote_spanned! {bi.field.span=>
+                        #hash_trait_path::hash(&#arg, #state);
                     })
                 }
             });
@@ -51,7 +54,7 @@ pub fn derive(input: &ast::Input) -> proc_macro2::TokenStream {
     let name = &input.ident;
     let generics = utils::build_impl_generics(
         input,
-        &hash_trait_path,
+        &hash_trait_path_input_span,
         needs_hash_bound,
         |field| field.hash_bound(),
         |input| input.hash_bound(),
@@ -61,8 +64,8 @@ pub fn derive(input: &ast::Input) -> proc_macro2::TokenStream {
     let hasher_ty_parameter = utils::hygienic_type_parameter(input, "__H");
     quote! {
         #[allow(unused_qualifications)]
-        impl #impl_generics #hash_trait_path for #name #ty_generics #where_clause {
-            fn hash<#hasher_ty_parameter>(&self, __state: &mut #hasher_ty_parameter)
+        impl #impl_generics #hash_trait_path_input_span for #name #ty_generics #where_clause {
+            fn hash<#hasher_ty_parameter>(&self, #state: &mut #hasher_ty_parameter)
                 where #hasher_ty_parameter: #hasher_trait_path
             {
                 #discriminant
@@ -79,11 +82,11 @@ fn needs_hash_bound(attrs: &attr::Field) -> bool {
 }
 
 /// Return the path of the `Hash` trait, that is `::std::hash::Hash`.
-fn hash_trait_path() -> syn::Path {
+fn hash_trait_path(span: proc_macro2::Span) -> TokenStream {
     if cfg!(feature = "use_core") {
-        parse_quote!(::core::hash::Hash)
+        quote_spanned!(span=> ::core::hash::Hash)
     } else {
-        parse_quote!(::std::hash::Hash)
+        quote_spanned!(span=> ::std::hash::Hash)
     }
 }
 
